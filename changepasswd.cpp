7f0b3baee1716754dc11e3ed08c820801dde26be
kangwenyun <798218157@qq.com>
2017-03-02 16:38:23 +0800

    changePasswd dbhelper

--- changepasswd.cpp
+++ changepasswd.cpp
@@ -2,6 +2,8 @@
 #include "ui_changepasswd.h"
 #include "session.h"
 #include "utils.h"
+#include "qres.h"
+#include "dbhelper.h"
 #include <QString>
 #include <QMessageBox>
 #include <QFile>
@@ -42,72 +44,72 @@ void ChangePasswd::on_ok_button_clicked()
             && !ui->newKey_edit->text().isEmpty()
             && !ui->newKeyAgain_edit->text().isEmpty())
     {
-        //检查原密码是否正确
-        //读取本地文件验证密码
-        //获取地址
-        QString txtName = "../batch_acquisition_system/reg.txt";
-        //定义文件对象
-        QFile file(txtName);
-        if(!file.open(QIODevice::ReadOnly))
-        {
-            QMessageBox::warning(this,tr("打开文件"),tr("打开失败"),file.errorString());
-            return;
-        }
-        bool ok = Utils::verifyPasswd(file,userId,ui->oldKey_edit->text());
-        //验证成功即可登录
-        if(!ok){
-            //提示错误
-            ui->err_label->setText(tr("原密码错误！"));
-            ui->err_label->show();
-            return;
-        }
-        //检查“新密码”与“确认新密码”是否相等
-        if(ui->newKey_edit->text() != ui->newKeyAgain_edit->text())
-        {
-            //提示错误
-            ui->err_label->setText(tr("请确认新密码！"));
-            ui->err_label->show();
-            return;
-        }
-        file.close();
-        //根据userid去文件查询相关数据删除并写入新信息
-        QFile fileIn(txtName);
-        if(!fileIn.open(QIODevice::ReadOnly)){
-            QMessageBox::warning(this,tr("打开文件"),tr("打开文件失败"),fileIn.errorString());
-            return;
-        }
-        QString fileInfo;
-        while(!fileIn.atEnd()){
-            QByteArray iLine = fileIn.readLine();
-            iLine = iLine.trimmed();
-            //需要将读出的一整行数据一同进行转码，而不能在分成list之后对某一项进行转码，会失败
-            QString infoLine = QString::fromLocal8Bit(iLine);
-            QList<QString> list = infoLine.split(',');
-            if(list[0] == userId){
-                //找到用户数据
-                //替换文件内容当前行
-                infoLine.clear();
-                infoLine = infoLine + list[0] + ',';
-                infoLine = infoLine + list[1] + ',';
-                infoLine = infoLine + ui->newKey_edit->text() + ',';
-                infoLine = infoLine + list[3] + ',';
-                infoLine = infoLine + list[4] + ',';
-                infoLine = infoLine + list[5] + '\n';
-            }
-            fileInfo += infoLine;
-        }
-        //删除源文件，写入新的同名文件
-        fileIn.remove();
-        QFile fileOut(txtName);
-        if(!fileOut.open(QIODevice::WriteOnly | QIODevice::Append | QIODevice::Text)){
-            QMessageBox::warning(this,tr("打开文件"),tr("打开文件失败"),fileOut.errorString());
-            return;
-        }
-        QByteArray fileContent = fileInfo.toLocal8Bit();
-        fileOut.write(fileContent);
-        //没有错误，修改密码成功
-        QMessageBox::information(this,tr("修改密码"),tr("修改密码成功！"));
-        return;
+//        检查原密码是否正确
+//        //读取本地文件验证密码
+//        //获取地址
+//        QString txtName = "../batch_acquisition_system/reg.txt";
+//        //定义文件对象
+//        QFile file(txtName);
+//        if(!file.open(QIODevice::ReadOnly))
+//        {
+//            QMessageBox::warning(this,tr("打开文件"),tr("打开失败"),file.errorString());
+//            return;
+//        }
+//        bool ok = Utils::verifyPasswd(file,userId,ui->oldKey_edit->text());
+//        //验证成功即可登录
+//        if(!ok){
+//            //提示错误
+//            ui->err_label->setText(tr("原密码错误！"));
+//            ui->err_label->show();
+//            return;
+//        }
+//        //检查“新密码”与“确认新密码”是否相等
+//        if(ui->newKey_edit->text() != ui->newKeyAgain_edit->text())
+//        {
+//            //提示错误
+//            ui->err_label->setText(tr("请确认新密码！"));
+//            ui->err_label->show();
+//            return;
+//        }
+//        file.close();
+//        //根据userid去文件查询相关数据删除并写入新信息
+//        QFile fileIn(txtName);
+//        if(!fileIn.open(QIODevice::ReadOnly)){
+//            QMessageBox::warning(this,tr("打开文件"),tr("打开文件失败"),fileIn.errorString());
+//            return;
+//        }
+//        QString fileInfo;
+//        while(!fileIn.atEnd()){
+//            QByteArray iLine = fileIn.readLine();
+//            iLine = iLine.trimmed();
+//            //需要将读出的一整行数据一同进行转码，而不能在分成list之后对某一项进行转码，会失败
+//            QString infoLine = QString::fromLocal8Bit(iLine);
+//            QList<QString> list = infoLine.split(',');
+//            if(list[0] == userId){
+//                //找到用户数据
+//                //替换文件内容当前行
+//                infoLine.clear();
+//                infoLine = infoLine + list[0] + ',';
+//                infoLine = infoLine + list[1] + ',';
+//                infoLine = infoLine + ui->newKey_edit->text() + ',';
+//                infoLine = infoLine + list[3] + ',';
+//                infoLine = infoLine + list[4] + ',';
+//                infoLine = infoLine + list[5] + '\n';
+//            }
+//            fileInfo += infoLine;
+//        }
+//        //删除源文件，写入新的同名文件
+//        fileIn.remove();
+//        QFile fileOut(txtName);
+//        if(!fileOut.open(QIODevice::WriteOnly | QIODevice::Append | QIODevice::Text)){
+//            QMessageBox::warning(this,tr("打开文件"),tr("打开文件失败"),fileOut.errorString());
+//            return;
+//        }
+//        QByteArray fileContent = fileInfo.toLocal8Bit();
+//        fileOut.write(fileContent);
+        dbhelper helper;
+        Qres res =  helper.Qchangepwd(userId,ui->oldKey_edit->text(),ui->newKey_edit->text());
+        QMessageBox::information(this,tr("修改密码"),res.msg);
     }else{
         QMessageBox::information(this,tr("错误提示"),tr("所有项均为必填项！"));
     }
